4af7cd17cd4b5a53f7201e0b229d80a3dc4b88ce
Wojciech Siewierski <darkvifon@gmail.com>
2012-04-25 09:27:28 +0200

    better install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 mkdir $HOME/.zplugins
 for f in zplugins/*; do
-    ln -s "`readlink -e "$f"`" "$HOME/.zplugins"
+    ln -v -s "`readlink -e "$f"`" "$HOME/.zplugins"
 done
-ln -s "`readlink -e zlogin`"  $HOME/.zlogin
-ln -s "`readlink -e zlogout`" $HOME/.zlogout
-ln -s "`readlink -e zloader`" $HOME/.zloader
-ln -s "`readlink -e zprompt`" $HOME/.zprompt
-ln -s "`readlink -e zshrc`"   $HOME/.zshrc
+ln -v -s "`readlink -e zlogin`"  $HOME/.zlogin
+ln -v -s "`readlink -e zlogout`" $HOME/.zlogout
+ln -v -s "`readlink -e zloader`" $HOME/.zloader
+ln -v -s "`readlink -e zprompt`" $HOME/.zprompt
+ln -v -s "`readlink -e zshrc`"   $HOME/.zshrc
