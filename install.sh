c9eccfe41044c86829befaecd6361b179a465522
Wojciech Siewierski <darkvifon@gmail.com>
2012-09-17 22:19:53 +0200

    better install script

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+
+LN="ln"
+
+if [ "$1" = "--overwrite" ]; then
+    LN="ln -f"
+fi
+
 mkdir -p $HOME/.zplugins
 for f in zplugins/*; do
-    ln -s "`readlink -e "$f"`" "$HOME/.zplugins" 2> /dev/null &&
+    $LN -s "`readlink -e "$f"`" "$HOME/.zplugins" 2> /dev/null &&
     echo "[32;1m$f[0m" 1>&2 ||
     echo "[31;1m$f[0m" 1>&2
 done
-for f in zlogin zlogout zloader zprompt zshrc; do
-    ln -s "`readlink -e "$f"`" "$HOME/.$f" 2> /dev/null &&
+for f in zlogin zlogout zloader zprompt zshrc zshenv; do
+    $LN -s "`readlink -e "$f"`" "$HOME/.$f" 2> /dev/null &&
     echo "[32;1m$f[0m" 1>&2 ||
     echo "[31;1m$f[0m" 1>&2
 done
