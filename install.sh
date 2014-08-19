7397ba043956862d48ab8c1cd43a972701b14ed7
Wojciech Siewierski <darkvifon@gmail.com>
2014-08-19 20:05:05 +0200

    install script update

--- install.sh
+++ install.sh
@@ -2,30 +2,36 @@
 
 LN="ln"
 
-while getopts "f" ARG; do
+while getopts "fs" ARG; do
     case "$ARG" in
         f)
             LN="ln -f" ;;
+        s)
+            git submodule update --init
+            ;;
         ?)
             ;;
     esac
 done
 shift $[$OPTIND-1]
 
+link_fun()
+{
+    local OUTPUT
+    OUTPUT=$($=LN -sv $1 $2 2>&1)
+    if [ "$?" = "0" ]; then
+        echo "[32;1m$OUTPUT[0m" 1>&2
+    else
+        echo "[31;1m$OUTPUT[0m" 1>&2
+    fi
+}
+
 mkdir -p $HOME/.zplugins
 for f in zplugins/*; do
-    OUTPUT=$($=LN -sv $f:A $HOME/.zplugins 2>&1)
-    if [ "$?" = "0" ]; then
-        echo "[32;1m$OUTPUT[0m" 1>&2
-    else
-        echo "[31;1m$OUTPUT[0m" 1>&2
-    fi
+    link_fun $f:A $HOME/.zplugins
 done
 for f in zlogin zlogout zloader zprompt zshrc zshenv; do
-    OUTPUT=$($=LN -sv $f:A $HOME/.$f 2>&1)
-    if [ "$?" = "0" ]; then
-        echo "[32;1m$OUTPUT[0m" 1>&2
-    else
-        echo "[31;1m$OUTPUT[0m" 1>&2
-    fi
+    link_fun $f:A $HOME/.$f
 done
+
+link_fun $HOME/.zplugins/fasd.d/fasd $HOME/.bin
